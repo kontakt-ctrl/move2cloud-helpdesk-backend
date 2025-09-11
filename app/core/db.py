db5fad9b2f92510354d65de42594c5f6b34cf2c4
kontakt-ctrl <kontakt@move2.cloud>
2025-09-11 08:35:35 +0200

    Update db.py

--- app/core/db.py
+++ app/core/db.py
@@ -1,12 +1,10 @@
-from sqlmodel import SQLModel, create_engine
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel, create_engine, Session
 from app.core.config import settings
 
 DATABASE_URL = settings.database_url
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+SessionLocal = Session
 
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+def init_db():
+    with engine.begin() as conn:
+        SQLModel.metadata.create_all(conn)
